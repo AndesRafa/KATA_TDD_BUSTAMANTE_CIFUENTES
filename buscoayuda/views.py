7c9edbaed309e9ebf340134a6b2674d60c995142
Rafael Bustamante <r.bustamante32@uniandes.edu.co>
2017-10-15 23:39:01 -0500

    Paso 2 (Verde) Se agrego funcionalidad de registro de independiente

--- buscoayuda/views.py
+++ buscoayuda/views.py
@@ -1,13 +1,51 @@
+from django.conf import settings
 from django.shortcuts import render
 
-# Create your views here.
-def index(request):
-    # trabajadores = Trabajador.objects.all()
-    # tipos_de_servicios = TiposDeServicio.objects.all()
-    # form_trabajador = TrabajadorForm(request.POST)
-    # form_usuario = UserForm(request.POST)
+from django.contrib.auth.models import User
+from django.http import HttpResponseRedirect
 
-    # context = {'trabajadores': trabajadores, 'tipos_de_servicios': tipos_de_servicios,
-    #            'form_trabajador': form_trabajador, 'form_usuario': form_usuario, 'base_url': settings.STATIC_URL}
-    # return render(request, 'buscoayuda/index.html', context)
-    return render(request, 'buscoayuda/index.html', None)
+from .models import TiposDeServicio, Trabajador, TrabajadorForm, UserForm
+
+# Create your views here.
+
+
+def index(request):
+    trabajadores = Trabajador.objects.all()
+    tipos_de_servicios = TiposDeServicio.objects.all()
+    form_trabajador = TrabajadorForm(request.POST)
+    form_usuario = UserForm(request.POST)
+
+    context = {'trabajadores': trabajadores,
+               'tipos_de_servicios': tipos_de_servicios,
+               'form_trabajador': form_trabajador,
+               'form_usuario': form_usuario,
+               'base_url': settings.STATIC_URL}
+
+    return render(request, 'buscoayuda/index.html', context)
+
+
+def register(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = User.objects.create_user(username=username, password=password)
+        user.first_name = request.POST.get('nombre')
+        user.last_name = request.POST.get('apellidos')
+        user.email = request.POST.get('correo')
+        user.save()
+
+        nuevo_trabajador = Trabajador(nombre=request.POST['nombre'],
+                                      apellidos=request.POST['apellidos'],
+                                      aniosExperiencia=request.POST.get(
+                                          'aniosExperiencia'),
+                                      tiposDeServicio=TiposDeServicio.objects.get(
+                                          pk=request.POST.get('tiposDeServicio')),
+                                      telefono=request.POST.get('telefono'),
+                                      correo=request.POST.get('correo'),
+                                      imagen=request.FILES['imagen'],
+                                      usuarioId=user
+                                      )
+        nuevo_trabajador.save()
+
+    return HttpResponseRedirect('/')
